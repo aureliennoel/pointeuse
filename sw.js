ee92037995be3f3c1e20c132c12b6929b108fc0d
aureliennoel <138508283+aureliennoel@users.noreply.github.com>
2026-02-22 19:54:48 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
-const CACHE="pointeuse-v1";
+const CACHE = "pointeuse-cache-v3";
+const ASSETS = ["./", "./index.html", "./app.js", "./manifest.webmanifest"];
 
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>
-      c.addAll([
-        "./",
-        "./index.html",
-        "./app.js",
-        "./manifest.webmanifest"
-      ])
-    )
-  );
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
